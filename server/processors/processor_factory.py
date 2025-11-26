0c975189e3ff411ac5ea7c9d3dde2d60ab58e7ce
KateKovzalenko <85827530+KateKovzalenko@users.noreply.github.com>
2025-11-26 08:12:54 +0100

    Clean mediapipe code (#8)

--- server/processors/processor_factory.py
+++ server/processors/processor_factory.py
@@ -1,17 +1,13 @@
 from enum import Enum
-from .mediapipe_processor import MediapipeProcessor
 from .hamer_processor import HamerProcessor
 
 class ProcessorType(Enum):
-    MEDIAPIPE = "mediapipe"
     HAMER = "hamer"
 
 
 def create_processor(processor_type: ProcessorType):
     """Create a processor instance based on ProcessorType enum."""
-    if processor_type == ProcessorType.MEDIAPIPE:
-        return MediapipeProcessor()
-    elif processor_type == ProcessorType.HAMER:
+    if processor_type == ProcessorType.HAMER:
         return HamerProcessor()
     else:
         raise ValueError(f"Unsupported processor type: {processor_type}")
